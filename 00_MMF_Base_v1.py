174356b9f9f16d75579d4dfdc8ddaec437139ddd
Fuatai-Rose <78947577+Fuatai-Rose@users.noreply.github.com>
2022-03-23 10:44:55 +1300

    Add files via upload

--- 00_MMF_Base_v1.py
+++ 00_MMF_Base_v1.py
@@ -1,7 +1,27 @@
 #Import statements
-
-
+print("Hello World")
 #functions go here
 
+# * * * * * * * * Main Routine * * * * * * * *
 
-# * * * * * * * * * * * Main
+# Set up dictionaries / lists needed to hold data
+
+#ask user if they have used the program before and show instructions if necessary
+
+#Loop to get ticket details
+
+    #Get name (Can't be blank)
+
+    # Get age (Between 12 and 130)
+
+    #Calculate ticket price
+
+    #Loop to ask for snacks
+
+    #Calculate snack price
+
+    #Ask for payment method (and apply surcharge if necessary)
+
+#Calculate Total sales and profit
+
+#Output data to text file
